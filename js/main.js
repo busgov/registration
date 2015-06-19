8b75966118b6a03bc0d80625c40e697d020dedd7
bensonbabu <benson.babu@gmail.com>
2015-06-19 16:53:36 +1000

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-/* Progress meter animation */
+/* Progress meter animation 
 
-/*
+
 $(function() {
     $(".meter > span").each(function() {
         $(this)
@@ -15,35 +15,67 @@ $(function() {
 
 
 /* Slide out panel */
-
+var steps = new Array(
+    ["Sole Trader", "sole_trader"], 
+    ["Business structure", "business_structure"],
+    ["Goods and Services Tax (GST)", "gst"],
+    ["Business name", "business_name"],
+    ["Other tax registrations", "other_tax"]
+    );
+var step = 0;
+var maxStep = 5;
 jQuery(document).ready(function($){
-	$('.cd-btn').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panel').addClass('is-visible');
-	});
-	$('.cd-panel').on('click', function(event){
-		if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
-			$('.cd-panel').removeClass('is-visible');
-			event.preventDefault();
-		}
-	});
+  loadQuestionHelp();
+
+  $("#previous").click(function() { step--;loadQuestionHelp(); });
+  $("#next").click(function() {step++; loadQuestionHelp(); });
 });
 
-
-/* Expand collapse headings config */
-
-new jQueryCollapse($(".showhide"), {
-  open: function() {
-    this.slideDown(150);
-  },
-  close: function() {
-    this.slideUp(150);
+function loadQuestionHelp()
+{
+  if(step < 0)
+  {
+    window.location.href = "home.html";
   }
-});
+  var templateDirectory = "templates/"
+  $("#heading").html(steps[step][0]);
+  $("#stepNo").html(step + 1);
+  var percentCompleted = step / maxStep * 100;
+  $("#percentCompleted").html(percentCompleted);
+  if(percentCompleted == 0)
+  {
+    percentCompleted+= 2; 
+  }
+  $("#percentMeter").css('width', percentCompleted + '%');
+  $("#question").load(templateDirectory + steps[step][1] + "_question.html");
+  $("#helpfile").load(templateDirectory + steps[step][1] + "_help.html", function() {setTimeout(applyStyle, 0);});  
+}
 
+function applyStyle()
+{
+  $('.cd-btn').on('click', function(event){
+        event.preventDefault();
+        $('.cd-panel').addClass('is-visible');
+      });
+  $('.cd-panel').on('click', function(event){
+    if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
+      $('.cd-panel').removeClass('is-visible');
+      event.preventDefault();
+    }
+  });
+  
+  /* Expand collapse headings config */
+  new jQueryCollapse($(".showhide"), {
+    open: function() {
+      this.slideDown(150);
+    },
+    close: function() {
+      this.slideUp(150);
+    }
+  });
+}
 
 /* show or hide form fields */
-
 $(document).ready(
     function(){
       $("input[name=payg]").click(function(){
