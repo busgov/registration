02f28d8e405d8de608f46ccebe7bce9dc2c13ac5
dssindustry <david.shaw@industry.gov.au>
2016-05-12 12:26:15 +1000

    javascript code for input type=file controls

--- business-registration/assets/js/file-inputs.js
+++ business-registration/assets/js/file-inputs.js
@@ -4,33 +4,32 @@
 */
 
 'use strict';
+$(document).ready(function() {
+    $(".custom-controls input[type=file]").each(function() {
+        var $input = $(this),
+            $label = $input.next("label"),
+            labelVal = $label.html();
 
-;( function( $, window, document, undefined )
-{
-	$( 'input[type=file]' ).each( function()
-	{
-		var $input	 = $( this ),
-			$label	 = $input.next( 'label' ),
-			labelVal = $label.html();
+        $label.html($("<strong></strong>").text(labelVal));
+        $label.prepend($("<span></span>"));
 
-		$input.on( 'change', function( e )
-		{
-			var fileName = '';
+        $input.on("change", function(e) {
+            var fileName = "";
 
-			if( this.files && this.files.length > 1 )
-				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-			else if( e.target.value )
-				fileName = e.target.value.split( '\\' ).pop();
+            if (this.files && this.files.length > 1)
+                fileName = (this.getAttribute("data-multiple-caption") || "").replace("{count}", this.files.length);
+            else if (e.target.value)
+                fileName = e.target.value.split("\\").pop();
 
-			if( fileName )
-				$label.find( 'span' ).html( fileName );
-			else
-				$label.html( labelVal );
-		});
+            if (fileName)
+                $label.find("span").html(fileName);
+            else
+                $label.html(labelVal);
+        });
 
-		// Firefox bug fix
-		$input
-		.on( 'focus', function(){ $input.addClass( 'has-focus' ); })
-		.on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
-	});
-})( jQuery, window, document );
+        // Firefox bug fix
+        $input
+            .on("focus", function() { $input.addClass("has-focus"); })
+            .on("blur", function() { $input.removeClass("has-focus"); });
+    });
+});
