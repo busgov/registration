56b38229a1df8d105373d5c657f51066a3a57ed7
dssindustry <david.shaw@industry.gov.au>
2016-10-04 10:40:12 +1100

    allow for multiple slide-outs

--- business-registration/assets/js/functions.js
+++ business-registration/assets/js/functions.js
@@ -94,37 +94,65 @@ function notifyGAofHelpRequest(href) {
     }
 }
 
-function initHelp() {
+function initHelp(helpID, linkClass) {
+    // default values when none provided:
+    helpID = helpID ? helpID : "#help";
+    linkClass = linkClass ? linkClass : ".cd-btn, .cd-help-link";
+
     /* Expand collapse headings config */
-    var help = new jQueryCollapse($("#help").find(".showhide"), {
+    var help = new jQueryCollapse($(helpID).find(".showhide"), {
+        query: 'h3',
         open: function () {
-            this.slideDown(1500);
-            // $("#helpTopic").focus();
+            this.slideDown(150);
         },
         close: function () {
-            this.slideUp(1500);
+            this.slideUp(150);
         }
     });
-    $("#help").find('.cd-panel').on('click', function (event) {
+    $(helpID).find('.cd-panel').on('click', function (event) {
         if ($(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close')) {
             help.close();
-            $("#help").find('.cd-panel').removeClass('is-visible');
+            $(helpID).find('.cd-panel').removeClass('is-visible');
             event.preventDefault();
-            $('#settings').toggle();
+            help.element.focus();
         }
     });
-    $('.cd-btn').on('click', function (event) {
-		alert(0);
-		notifyGAofHelpRequest(event);
-        var index = $("a.cd-btn").index(this);
+    $(linkClass).on('click', function (event) {
+        help.element = $(this);
+        var href = $(this).attr("href");
+        notifyGAofHelpRequest(href);
+        var helpAnchors = $(helpID + " .help-anchor");
+        var anchor = helpAnchors.eq(helpAnchors.index($(href)));
+        var section = anchor.nextAll("h3").first();
+        var index = $(helpID + " h3").index($(section));
+        //var index = $(helpID + " .help-anchor").index($(href));
+        $(helpID + " .cd-panel-content").scrollTop(0);
         help.open(index);
-		$("#helpfile h3 a").eq(index).focus();
         event.preventDefault();
-        $("#help").find('.cd-panel').addClass('is-visible');
-        $('#settings').toggle();
+        $(helpID).find('.cd-panel').addClass('is-visible').promise().done(function() {
+            window.setTimeout(function () {
+                scrollInParent($(helpID + " h3.open"));
+                $(helpID + " h3.open a").focus();
+            }, 500);
+        });
+    });
+
+    $(helpID).keydown(function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 9) {
+            var links = $(helpID).find("a:visible");
+            var index = links.index(e.target);
+            index += e.shiftKey ? -1 : 1;
+            //alert($(e.target).html() + ": " + index);
+            if (index < 0 || index >= links.length) {
+                help.close();
+                $(helpID).find('.cd-panel').removeClass('is-visible');
+                e.preventDefault();
+                help.element.focus();
+            }
+        }
     });
 }
-
 function initSaveForLater() {
     $("#saveForLater").load("save-for-later-content.html?t=" + (new Date()).getTime(), function () {
         setTimeout(function() {
